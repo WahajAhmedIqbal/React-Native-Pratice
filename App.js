e84e20b067290c4f911c228b7b3c69bc260dfbe5
WahajAhmedIqbal <wahaj465@gmail.com>
2020-10-19 19:01:32 +0500

    first

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  console.log("React natice");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding=50}}>
+      <View style={{flexDirection= 'row', justifyContent: "space-between", alignItems="center"}}>
+        <TextInput placeholder='Add Item' style={{borderBottomColor: 'black', borderBottonWidth:1}}/>
+        <Button title="Add" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
