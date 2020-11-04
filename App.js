ae0c307fd116335fdaff5b897328062434094d7e
WahajAhmedIqbal <wahaj465@gmail.com>
2020-11-05 04:32:04 +0500

    2.13 todo list add item

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+  Button,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log("React natice");
+  const [todolist, setTodolist] = useState("");
+  const [addTodo, setAddTodo] = useState([]);
+
+  const handleTodoList = (inputtext) => {
+    setTodolist(inputtext);
+  };
+  const handleAdd = () => {
+    setAddTodo((currentstate) => [...currentstate, todolist]);
+  };
+
   return (
-    <View>
-      <Text>Hello world</Text>
-      <Text>Hello world</Text>
-      <Text>Hello world</Text>
-      <Text>Hello world</Text>
-      <Text>Hello world</Text>
-      <Text>Hello world</Text>
-      <Text>Hello world</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Enter Items"
+          style={{
+            width: "80%",
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+          }}
+          onChangeText={handleTodoList}
+          value={todolist}
+        />
+        <Button title="ADD" onPress={handleAdd} />
+      </View>
+      <View>
+        <Text>this is for testig </Text>
+      </View>
+      <ScrollView>
+        {addTodo.map((items) => (
+          <View style={styles.listitem}>
+            <Text>{items}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listitem: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
